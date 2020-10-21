7b0b81e6a96f959b9720b343a89d59165e750544
smalllong <m18818276304@icloud.com>
2020-10-21 17:58:32 +0800

    finish 1.1.6

---
+++ 1.1/6.js
@@ -0,0 +1,44 @@
+// 1.1
+10; //10
+
+5 + 3 + 4; //12
+
+9 - 1; //8
+
+6 / 2; //3
+
+2 * 4 + (4 - 6); //6
+
+const a = 3;
+
+const b = a + 1;
+
+a + b + a * b; //19
+
+a === b; //false
+
+b > a && b < a * b
+    ? b : a; //4
+
+a === 4 ? 6 : b === 4 ? 6 + 7 + a : 25; //16
+
+// 1.2
+(5 + 4 + (2 - (3 - (6 + 4 / 5)))) / (3 * (6 - 2) * (2 - 7))
+
+// 1.3
+function sumOfLargerSquares(a, b, c) {
+    return a < b && a < c
+    ? b * b + c * c
+    : b < c
+        ? a * a + c * c
+        : a * a + b * b;
+}
+
+// 1.4
+function a_plus_abs_b(a, b) {
+    return (b >= 0 ? a + b : a - b);
+}
+
+// 1.5
+// applicative-order evaluation: infinite loop
+// normal-order evaluation: 0
